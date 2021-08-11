a27988ed33fb1aa2f0c5d01d78ee154071047deb
SelyanKab <selyan.kab@protonmail.com>
2021-08-11 09:23:42 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 
+import time
+
+import pyodbc
+print('Entrez le nom:  ')
+x = input()
+
+print('Entrez le prénom:  ')
+y = input()
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=Aselkim;'
+                      'Database=carte;'
+                      'Trusted_Connection=yes;')
+
+cursor = conn.cursor()
+cursor.execute('select nom_f, pren_f, d_nais, l_nais, annee, nord, ncni, d_etab  from pers where nom_f = ? and pren_f '
+               '= ?', x, y)
+for row in cursor.fetchall():
+    print(row)
+
+cursor2 = conn.cursor()
+cursor2.execute('select nom_f, pren_f, d_nais, l_nais, annee, nord, ncni, d_etab  from pers where nom_f = ? and pren_f '
+                '= ?', x, y)
+rows = cursor2.fetchall()
+print('Le nombre de personnes trouvées est:')
+print(len(rows))
+
+
+print('*****************************************************')
+print('Cliquez sur une touche pour quitter')
+time.sleep(59)
+print('Terminé')
